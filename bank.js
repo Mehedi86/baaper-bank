752e56ef6a5d4e54afe10857fda01e0dc321115a
Mehedi86 <abdullahalmehedi86@gmail.com>
2023-02-12 19:28:32 +0600

    final

--- bank.js
+++ bank.js
@@ -16,18 +16,27 @@ document.getElementById('btn-deposit').addEventListener('click',function(){
 
 document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawField =document.getElementById('withdraw-field');
-    const newWithdrawAmountstring =withdrawField.value;
+    const newWithdrawAmountstring =withdrawField.value;  
     const newwithdrawAmount = parseFloat(newWithdrawAmountstring);
+    withdrawField.value = '';
+    if(isNaN(newWithdrawAmountstring)){
+        alert('Please, provide a valid input!!!');
+        return;
+    }
+    const Balance =document.getElementById('balance-total');
+    const balanceString =Balance.innerText;
+    const balance =parseFloat(balanceString);
+    if(balance < newwithdrawAmount){
+        alert('Do not have sufficient balance');
+        return;
+    }
+    else{
     const withdrawTotalElement =document.getElementById('withdraw-total');
     const previousWithdrawAmountString =withdrawTotalElement.innerText;
     const previousWithdrawAmount =parseFloat(previousWithdrawAmountString);
     const totalWithdrawAmount =newwithdrawAmount+previousWithdrawAmount;
     withdrawTotalElement.innerText = totalWithdrawAmount;
-    withdrawField.value = '';
-    const Balance =document.getElementById('balance-total');
-    const balanceString =Balance.innerText;
-    const balance =parseFloat(balanceString);
     const totalBalance = balance - newwithdrawAmount;
     Balance.innerText = totalBalance;
-
+    }
 })
